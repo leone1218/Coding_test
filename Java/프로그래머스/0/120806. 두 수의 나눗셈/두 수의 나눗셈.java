b28fba2215ddf72bb2f99e7c0b8690eb2899daa7
leone1218 <leone1218@kakao.com>
2026-03-26 22:04:36 +0900

    [level 0] Title: 두 수의 나눗셈, Time: 0.02 ms, Memory: 72.5 MB -BaekjoonHub

---
+++ Java/프로그래머스/0/120806. 두 수의 나눗셈/두 수의 나눗셈.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int solution(int num1, int num2) {
+        int answer = 0;
+        double Anum1 = num1;
+        double Anum2 = num2;
+        answer =(int)(Anum1 / Anum2 * 1000);
+        
+        return answer;
+    }
+}
